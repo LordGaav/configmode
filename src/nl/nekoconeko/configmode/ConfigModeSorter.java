e791bcf438568697771758071c0952ecd24dc374
Nick Douma <n.douma@nekoconeko.nl>
2013-08-08 17:10:21 +0200

    Make ConfigModeSorter use base Option class instead of ConfigParameter

--- src/nl/nekoconeko/configmode/ConfigModeSorter.java
+++ src/nl/nekoconeko/configmode/ConfigModeSorter.java
@@ -20,10 +20,12 @@ package nl.nekoconeko.configmode;
 
 import java.util.Comparator;
 
+import org.apache.commons.cli.Option;
+
 public class ConfigModeSorter {
-	public static final Comparator<ConfigParameter> CONFIGPARAMETER_ALPHANUM = new Comparator<ConfigParameter>() {
+	public static final Comparator<Option> CONFIGPARAMETER_ALPHANUM = new Comparator<Option>() {
 		@Override
-		public int compare(ConfigParameter o1, ConfigParameter o2) {
+		public int compare(Option o1, Option o2) {
 			String o1s = String.format("%s%s", (o1.getOpt() == null ? "" : o1.getOpt()), o1.getLongOpt());
 			String o2s = String.format("%s%s", (o2.getOpt() == null ? "" : o2.getOpt()), o2.getLongOpt());
 			return o1s.compareTo(o2s);
